bc7c14fad711e6c19d94166f523779e6feb811ce
Ralph <ralph.waldenmaier@gmail.com>
2015-02-10 21:48:41 +0100

    Finalizing plot1

--- Exploratory Data Analysis/Course Project 2/plot1.R
+++ Exploratory Data Analysis/Course Project 2/plot1.R
@@ -1,8 +1,16 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+if (!require("dplyr")) {
+  install.packages("dplyr")
+}
+
+	
+require("dplyr")
+	
 data <- group_by(NEI, year) %>% summarise(sm=sum(Emissions))
 
+png(file ="plot1.png", bg="white", width=480, height=480)
 op <- par(mar = c(5,8,4,2) + 0.1)
 with(data, plot(year, sm, type="b", pch=16, xlab="Year", ylab="", lwd=2, col="orangered", axes=FALSE))
 axis(1, at = data$year, labels = data$year)
@@ -11,3 +19,5 @@ title(ylab = "Emmissions (tons)", line = 6)
 box()
 title(main = "Summarized emissions by year")
 par(op)
+
+dev.off()
