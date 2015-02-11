b9b91eff06f8fa1e861c56eabb628bb4fc10cd0b
Ralph <ralph.waldenmaier@gmail.com>
2015-02-11 20:51:53 +0100

    Adjusting plots

--- Exploratory Data Analysis/Course Project 2/plot2.R
+++ Exploratory Data Analysis/Course Project 2/plot2.R
@@ -19,7 +19,7 @@ op <- par(mar = c(5,8,4,2) + 0.1)
 with(data, plot(year, sm, type="b", pch=16, xlab="Year", ylab="", lwd=2, col="orangered", axes=FALSE))
 axis(1, at = data$year, labels = data$year)
 axis(2, at = data$sm, labels=format(data$sm, digits=2, nsmall=0, big.mark=","), las=1)
-title(ylab = "Emmissions (tons)", line = 6)
+title(ylab = "PM2.5 (tons)", line = 6)
 box()
 title(main = "Summarized emissions by year for Baltimore City")
 
--- Exploratory Data Analysis/Course Project 2/plot3.R
+++ Exploratory Data Analysis/Course Project 2/plot3.R
@@ -18,13 +18,32 @@ require("ggplot2")
 	
 data <- filter(NEI, fips=="24510") %>% group_by(type, year) %>% summarise(sm=sum(Emissions))
 
-png(file ="plot3.png", bg="white", width=800, height=480)
-g <- ggplot(data, aes(year, sm)) +
-	geom_line(color="steelblue", size=2, alpha=1/8) +
-	geom_point(color="steelblue", size = 4, alpha = 1/2)  +
-	facet_grid(~type) +
-	labs(title = "Summarized emissions by year for Baltimore City per type") +
-	labs(x = "Year", y = "Emmissions (tons)")
+png(file ="plot3.png", bg="white", width=480, height=480)
+
+# good solid solution
+#g <- ggplot(data, aes(year, sm)) +
+#	geom_line(color="steelblue", size=2, alpha=1/8) +
+#	geom_point(color="steelblue", size = 4, alpha = 1/2)  +
+#	#facet_grid(~type) +
+#	facet_wrap(~type, ncol=1) +
+#	labs(title = "Summarized emissions by year for Baltimore City per type") +
+#	labs(x = "Year", y = "Emmissions (tons)")
+	
+g <- ggplot(data, aes(year, sm, color=factor(type))) +
+	geom_line(size=2, alpha=1/4) +
+	geom_point(size = 4, alpha = 1/2)  +	
+	facet_wrap(~type, ncol=1) +	
+	labs(title = "Summarized emissions by year for 'Baltimore City' per type") +
+	labs(x = "Year", y = "PM2.5 (tons)") +
+	labs(colour = "Types")
+
+
+# one chart with different lines inside	
+#g <- ggplot(data, aes(year, sm, color=factor(type))) +
+#	geom_line( size=2, alpha=1/8) +
+#	geom_point(size = 4, alpha = 1/2)  +
+#	labs(title = "Summarized emissions by year for Baltimore City per type") +
+#	labs(x = "Year", y = "Emmissions (tons)")
 
 print(g)	
 dev.off()
