4ba872816b13bcd5015a5dc4c061f06834c413b0
Ralph <ralph.waldenmaier@gmail.com>
2015-02-11 19:21:28 +0100

    Checking for existing data; Adding plot2

--- Exploratory Data Analysis/Course Project 2/plot2.R
+++ Exploratory Data Analysis/Course Project 2/plot2.R
@@ -11,16 +11,18 @@ if (!require("dplyr")) {
 	
 require("dplyr")
 	
-data <- group_by(NEI, year) %>% summarise(sm=sum(Emissions))
+data <- filter(NEI, fips=="24510") %>% group_by(year) %>% summarise(sm=sum(Emissions))
 
-png(file ="plot1.png", bg="white", width=480, height=480)
+png(file ="plot2.png", bg="white", width=480, height=480)
+
 op <- par(mar = c(5,8,4,2) + 0.1)
 with(data, plot(year, sm, type="b", pch=16, xlab="Year", ylab="", lwd=2, col="orangered", axes=FALSE))
 axis(1, at = data$year, labels = data$year)
 axis(2, at = data$sm, labels=format(data$sm, digits=2, nsmall=0, big.mark=","), las=1)
 title(ylab = "Emmissions (tons)", line = 6)
 box()
-title(main = "Summarized emissions by year")
+title(main = "Summarized emissions by year for Baltimore City")
+
 par(op)
 
 dev.off()
